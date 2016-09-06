67c6c755b8cf4abeaf0aef782496edb2b2f2eb2e
harshasaranam <harshasaranam@gmail.com>
2016-09-06 16:05:48 -0500

    9/6

--- Lab-2/source/register.php
+++ Lab-2/source/register.php
@@ -1,23 +1,23 @@
 <?php
-$con = mysql_connect("localhost",“root","Fuckdude.0032");
-if (!$con)
-{
-die('Could not connect: ' . mysql_error());
+$con = mysqli_connect(3306,"root","Fuckdude.0032");
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-/*if (mysql_query(" CREATE DATABASE lab2",$con))
+echo "Connected successfully";
+if (mysqli_query(" CREATE DATABASE lab2",$con))
 echo "Database created";
 else
-echo "Error creating database: " . mysql_error();
+echo "Error creating database: " . mysqli_error();
 $sql="CREATE TABLE persons(firstname CHAR(30),lastname CHAR(30),emailid CHAR(30),password CHAR(30))";
-if (mysql_query($con,$sql))
+if (mysqli_query($con,$sql))
 {
 echo "Table persons created successfully";
 }
 else
 {
-echo "Error creating table: " . mysql_error();
-}*/
+echo "Error creating table: " . mysqli_error();
+}
 $sql="INSERT INTO persons(firstName, lastName, emailid, password)VALUES('$_POST[firstname]','$_POST[lastname]','$_POST[emailid]','$_POST[password]')";
+mysqli_close($con);
 
-mysql_close($con);
 ?>
